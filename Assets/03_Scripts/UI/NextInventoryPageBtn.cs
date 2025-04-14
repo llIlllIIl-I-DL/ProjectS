5bcbd2df31d36dc24240a85eb42527ce971d9c83
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-04-14 19:54:07 +0900

    [Feat] UI 시스템 진짜 완료

--- Assets/03_Scripts/UI/NextInventoryPageBtn.cs
+++ Assets/03_Scripts/UI/NextInventoryPageBtn.cs
@@ -5,20 +5,81 @@ using UnityEngine.UI;
 
 public class NextInventoryPageBtn : MonoBehaviour
 {
+    //리스트 혹은 딕셔너리
+    //인덱스에서 쁠마 1씩 넘무나 감사...
+
     [SerializeField] public Button invenSlotLeftBtn;
     [SerializeField] public Button invenSlotRightBtn;
 
     [SerializeField] public GameObject currentPage;
+    /*
     [SerializeField] public GameObject InventoryPage1;
     [SerializeField] public GameObject InventoryPage2;
     [SerializeField] public GameObject InventoryPage3;
+    */
+
+    [SerializeField] public List<GameObject> Pages = new List<GameObject>();
+
+    int i = 0;
 
     void Start()
     {
-        invenSlotLeftBtn.onClick.AddListener(() => MoveToLeftPage(currentPage));
-        invenSlotRightBtn.onClick.AddListener(() => MoveToRightPage(currentPage));
+        invenSlotLeftBtn.onClick.AddListener(() => LeftPage());
+        invenSlotRightBtn.onClick.AddListener(() => RightPage());
     }
 
+    public void LeftPage()
+    {
+        currentPage.SetActive(false);
+
+        currentPage = Pages[i + 2];
+
+        SwapLeft();
+
+        currentPage.SetActive(true);
+    }
+
+    public void SwapLeft()
+    {
+        GameObject Temp = Pages[0];
+        Pages[0] = Pages[2];
+        Pages[2] = Pages[1];
+        Pages[1] = Temp;
+    }
+
+
+
+
+    public void RightPage()
+    {
+        currentPage.SetActive(false);
+
+        currentPage = Pages[i + 1];
+
+        SwapRight();
+
+        currentPage.SetActive(true);
+
+    }
+
+    public void SwapRight()
+    {
+        GameObject Temp = Pages[1];
+        Pages[1] = Pages[2];
+        Pages[2] = Pages[0];
+        Pages[0] = Temp;
+    }
+
+
+
+
+
+
+
+
+
+
+    /*
     public void MoveToLeftPage(GameObject currentPage)
     {
         if (currentPage == InventoryPage1)
@@ -28,7 +89,6 @@ public class NextInventoryPageBtn : MonoBehaviour
 
             this.currentPage = InventoryPage3;
         }
-
 
         else if (currentPage == InventoryPage3)
         {
@@ -75,4 +135,5 @@ public class NextInventoryPageBtn : MonoBehaviour
             this.currentPage = InventoryPage1;
         }
     }
+    */
 }
