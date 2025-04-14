c5e143dab748bbd00123bff72bcf042f3df44b47
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-04-14 21:03:05 +0900

    [Feat] Page 넘기기 기능 수정

--- Assets/03_Scripts/UI/NextInventoryPageBtn.cs
+++ Assets/03_Scripts/UI/NextInventoryPageBtn.cs
@@ -5,135 +5,47 @@ using UnityEngine.UI;
 
 public class NextInventoryPageBtn : MonoBehaviour
 {
-    //리스트 혹은 딕셔너리
-    //인덱스에서 쁠마 1씩 넘무나 감사...
-
     [SerializeField] public Button invenSlotLeftBtn;
     [SerializeField] public Button invenSlotRightBtn;
 
-    [SerializeField] public GameObject currentPage;
-    /*
-    [SerializeField] public GameObject InventoryPage1;
-    [SerializeField] public GameObject InventoryPage2;
-    [SerializeField] public GameObject InventoryPage3;
-    */
-
     [SerializeField] public List<GameObject> Pages = new List<GameObject>();
 
-    int i = 0;
+    int currentPage = 0;
+    int totalPage;
 
     void Start()
     {
+        totalPage = Pages.Count;
+
         invenSlotLeftBtn.onClick.AddListener(() => LeftPage());
         invenSlotRightBtn.onClick.AddListener(() => RightPage());
     }
 
     public void LeftPage()
     {
-        currentPage.SetActive(false);
+        Pages[currentPage].SetActive(false);
 
-        currentPage = Pages[i + 2];
+        currentPage --;
 
-        SwapLeft();
+        if(currentPage < 0)
+        {
+            currentPage = totalPage -1;
+        }
 
-        currentPage.SetActive(true);
+        Pages[currentPage].SetActive(true);
     }
-
-    public void SwapLeft()
-    {
-        GameObject Temp = Pages[0];
-        Pages[0] = Pages[2];
-        Pages[2] = Pages[1];
-        Pages[1] = Temp;
-    }
-
-
-
 
     public void RightPage()
     {
-        currentPage.SetActive(false);
+        Pages[currentPage].SetActive(false);
 
-        currentPage = Pages[i + 1];
+        currentPage++; //식을 이렇게 하면 값을 영원히 저장한디야...
 
-        SwapRight();
+        if (currentPage >= totalPage)
+        {
+            currentPage = 0;
+        }
 
-        currentPage.SetActive(true);
-
+        Pages[currentPage].SetActive(true);
     }
-
-    public void SwapRight()
-    {
-        GameObject Temp = Pages[1];
-        Pages[1] = Pages[2];
-        Pages[2] = Pages[0];
-        Pages[0] = Temp;
-    }
-
-
-
-
-
-
-
-
-
-
-    /*
-    public void MoveToLeftPage(GameObject currentPage)
-    {
-        if (currentPage == InventoryPage1)
-        {
-            currentPage.SetActive(false);
-            InventoryPage3.SetActive(true);
-
-            this.currentPage = InventoryPage3;
-        }
-
-        else if (currentPage == InventoryPage3)
-        {
-            currentPage.SetActive(false);
-            InventoryPage2.SetActive(true);
-
-            this.currentPage = InventoryPage2;
-        }
-
-        else if (currentPage == InventoryPage2)
-        {
-            currentPage.SetActive(false);
-            InventoryPage1.SetActive(true);
-
-            this.currentPage = InventoryPage1;
-        }
-    }
-
-
-    public void MoveToRightPage(GameObject currentPage)
-    {
-        if (currentPage == InventoryPage1)
-        {
-            currentPage.SetActive(false);
-            InventoryPage2.SetActive(true);
-
-            this.currentPage = InventoryPage2;
-        }
-
-
-        else if (currentPage == InventoryPage2)
-        {
-            currentPage.SetActive(false);
-            InventoryPage3.SetActive(true);
-
-            this.currentPage = InventoryPage3;
-        }
-
-        else if (currentPage == InventoryPage3)
-        {
-            currentPage.SetActive(false);
-            InventoryPage1.SetActive(true);
-
-            this.currentPage = InventoryPage1;
-        }
-    }
-    */
 }
